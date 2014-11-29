a73843cc35b2261c4a7c185ac6f448c0904c1b3b
zigoni <zigoni@foxmail.com>
2014-11-29 10:13:51 +0800

    增加了删除序列中gap位点的函数

--- phylopylib/seq.py
+++ phylopylib/seq.py
@@ -1,3 +1,7 @@
+from collections import Counter
+from collections import OrderedDict
+
+
 # Convert gaps (-) in the beginning and the end to missing (?)
 def gap2missing(seq):
     length = len(seq)
@@ -12,3 +16,17 @@ def gap2missing(seq):
     seq = check(seq[::-1])
     seq = check(seq[::-1])
     return seq
+
+
+def delete_gap(seqs):
+    seq_length = len(list(seqs.values())[0])
+    seq_num = len(seqs)
+    r_seqs = OrderedDict(zip(seqs.keys(), ['']*seq_num))
+    for i in range(seq_length):
+        status = Counter(seqs[n][i] for n in seqs)
+        if status['-'] > 0:
+            continue
+        else:
+            for n in seqs:
+                r_seqs[n] += seqs[n][i]
+    return r_seqs
